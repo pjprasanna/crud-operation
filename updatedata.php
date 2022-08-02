30d8a6a480f0521645623bf12ea7500dfc08c46b
pjprasanna <89349460+pjprasanna@users.noreply.github.com>
2022-08-02 01:17:16 -0800

    Add files via upload

---
+++ updatedata.php
@@ -0,0 +1,14 @@
+<?php
+$stu_id = $_POST['sid'];
+$stu_name = $_POST['sname'];
+$stu_class = $_POST['sclass'];
+$stu_address = $_POST['saddress'];
+$stu_phone = $_POST['sphone'];
+
+include 'config.php';
+
+$sql = "UPDATE student SET sname = '{$stu_name}',sclass = '{$stu_class}', saddress = '{$stu_address}', sphone = '{$stu_phone}' WHERE sid = {$stu_id}";
+$result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+header("Location: http://localhost/crud/index.php");
+mysqli_close($conn);
+?>
